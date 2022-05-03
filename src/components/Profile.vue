ea6d1c4000bf00c0e83c495fefc9aa316a59fbe0
Hunny <devinhunny04021997@gmail.com>
2022-05-03 21:19:26 +0530

    upload profile code

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div
+    class="flex w-12 h-12 items-center justify-center bg-white shadow rounded-full"
+  >
+    <p class="text-xl font-bold">{{ name?.charAt(0) }}</p>
   </div>
 </template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  props: {
+    name: String,
+  },
   created() {
     // props are exposed on `this`
     console.log(this.$route);
   },
 });
 </script>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
