c40b816765da98f446d35126bd51e0b62b2c4743
Hunny <devinhunny04021997@gmail.com>
2022-05-14 19:07:46 +0700

    [ET-18] added test file

---
+++ src/__test__/NavBar.spec.js
@@ -0,0 +1,10 @@
+import { mount } from "@vue/test-utils";
+import NavBar from "../components/NavBar.vue";
+
+
+// The component to test
+
+test("displays message", () => {
+  const wrapper = mount(NavBar)
+ 
+});
---
+++ src/__test__/Profile.spec.js
@@ -0,0 +1,17 @@
+import { mount } from "@vue/test-utils";
+import Profile from "../components/Profile.vue";
+
+
+// The component to test
+
+test("displays message", () => {
+  const wrapper = mount(Profile, {
+    props: {
+      name: "Hello world",
+    },
+  });
+
+  // Assert the rendered text of the component
+  // const profilePara = wrapper.get('[testId="profile-text"]');
+  // expect(profilePara.text()).toBe("H");
+});
